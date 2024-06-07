8a201d722e0093f845685e2987c8ac445ffed5df
smhrd1 <smhrd1@smhrd>
2024-06-07 16:47:04 +0900

    0607/채팅기능/이지현

---
+++ ChatProgram/src/main/java/member/MemberDTO.java
@@ -0,0 +1,86 @@
+package member;
+
+public class MemberDTO {
+	private String mem_id;
+	private String mem_pw;
+	private String mem_name;
+	private String mem_phone;
+	private String mem_addr;
+	private String mem_nick;
+	private String mem_gender;
+	private String mem_birthdate;
+	private String mem_type;
+	private String joined_at;
+	private String mem_email;
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getMem_pw() {
+		return mem_pw;
+	}
+	public void setMem_pw(String mem_pw) {
+		this.mem_pw = mem_pw;
+	}
+	public String getMem_name() {
+		return mem_name;
+	}
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	public String getMem_phone() {
+		return mem_phone;
+	}
+	public void setMem_phone(String mem_phone) {
+		this.mem_phone = mem_phone;
+	}
+	public String getMem_addr() {
+		return mem_addr;
+	}
+	public void setMem_addr(String mem_addr) {
+		this.mem_addr = mem_addr;
+	}
+	public String getMem_nick() {
+		return mem_nick;
+	}
+	public void setMem_nick(String mem_nick) {
+		this.mem_nick = mem_nick;
+	}
+	public String getMem_gender() {
+		return mem_gender;
+	}
+	public void setMem_gender(String mem_gender) {
+		this.mem_gender = mem_gender;
+	}
+	public String getMem_birthdate() {
+		return mem_birthdate;
+	}
+	public void setMem_birthdate(String mem_birthdate) {
+		this.mem_birthdate = mem_birthdate;
+	}
+	public String getMem_type() {
+		return mem_type;
+	}
+	public void setMem_type(String mem_type) {
+		this.mem_type = mem_type;
+	}
+	public String getJoined_at() {
+		return joined_at;
+	}
+	public void setJoined_at(String joined_at) {
+		this.joined_at = joined_at;
+	}
+	public String getMem_email() {
+		return mem_email;
+	}
+	public void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+	
+
+	
+	
+
+}
